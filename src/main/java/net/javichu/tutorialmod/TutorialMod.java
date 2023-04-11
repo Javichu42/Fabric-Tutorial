03c2d5564df01d5c928775351c25a66cf32a86a4
Javichu <ljavi72467@gmail.com>
2023-04-11 23:01:17 +0200

    drill

--- src/main/java/net/javichu/tutorialmod/TutorialMod.java
+++ src/main/java/net/javichu/tutorialmod/TutorialMod.java
@@ -1,12 +1,13 @@
 package net.javichu.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.javichu.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -14,6 +15,6 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
